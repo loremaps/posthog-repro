5ddb0e02225b293504faa469655ec2d9d9f9754d
Panagiotis Kapros <kapros@gmail.com>
2021-08-08 13:47:17 +0300

    add repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import posthog from 'posthog-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'posthog-repro';
+
+  constructor(){
+    posthog.init('');
+  }
 }
